665eb441c9ce4c75b91de8fbe5e012feaea3a628
Stanislav Bogdanov <stanislav.tiryoshin@gmail.com>
2023-11-14 19:01:35 +0600

    added comments

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 AOS.init();
 var swiper = new Swiper(".swiper", {
-  // Optional parameters
+  // Направление
   direction: "horizontal",
+  // Конечность
   loop: true,
 
-  // If you want pagination
+  // Пагинация
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
 
+  // Скролл
   scrollbar: { el: ".swiper-scrollbar" },
 
+  // Расстояние между слайдами
   spaceBetween: 20,
 
+  // Количество слайдов в одной странице
   slidesPerView: 3,
 
+  // Кнопки навигации
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-});
-
-const submit = document.getElementById("submit");
-
-submit.addEventListener("submit", () => {
-  alert("Submitted");
 });
