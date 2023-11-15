1be150272f91bb64b457ecabc923aeb5e1d91418
Stanislav Bogdanov <stanislav.tiryoshin@gmail.com>
2023-11-15 18:59:20 +0600

    ready

--- counter.js
+++ counter.js
@@ -1,11 +1,11 @@
-const textarea = document.querySelector("textarea");
+const textarea = document.querySelector("#textarea");
 const wordsCounter = document.querySelector("#word-count");
 const charsCounter = document.querySelector("#char-count");
 
 textarea.addEventListener("input", () => {
   const input = textarea.value;
-  // console.log(input, "введеная строка");
-  // console.log(input.split(" "), "слова в строке");
+  console.log(input, "введеная строка");
+  console.log(input.split(" "), "слова в строке");
   const charsNumber = input.length;
   let wordsNumber;
   if (input === "") {
